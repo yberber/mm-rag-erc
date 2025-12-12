3bb4ac0d930a2c2ba56a50ab18ab580cd264745d
Yusuf Berber <yusufberber19@gmail.com>
2025-12-12 16:21:14 +0100

    delete unnecessary folders

--- TRAINING_DATA_CREATE/STAGE1/speaker_characteristics_dataset_generator.py
+++ TRAINING_DATA_CREATE/STAGE1/speaker_characteristics_dataset_generator.py
@@ -8,10 +8,9 @@ from TRAINING_DATA_CREATE.STAGE1 import llm_character_extraction, parallel_llm_c
 from utils import get_vectordb_path_from_attributes
 import asyncio  # 1. Import asyncio
 
-datasets = ["meld"]
+datasets = ["meld", "iemocap"]
 # prompt_types = ["default", "alt1", "alt2"]
-prompt_types = ["default-no-audio"]
-prompt_types = ["default"]
+# prompt_types = ["default-no-audio"]
 
 
 config = {
@@ -26,33 +25,16 @@ config = {
 
 config_list = []
 for dataset in datasets:
-    for prompt_type in prompt_types:
-
-        temp_config = config.copy()
-        temp_config['dataset_name'] = dataset
-        temp_config['prompt_type'] = prompt_type
-        config_list.append(temp_config)
-
-
-# config['prompt_type'] = 'default'
-# config['splits'] = None
-# for dataset in datasets:
-#     config['dataset_name'] = dataset
-#     config_list.append(config.copy())
-
-
-# for config in config_list[6:]:
-#     print(f"config: {config}")
-#     llm_character_extraction.main(config)
-#     print("*********************\n\n\n")
+    temp_config = config.copy()
+    temp_config['dataset_name'] = dataset
+    config_list.append(temp_config)
 
 async def main():
     for config in config_list:
         print(f"config: {config}")
-        # 3. Use 'await' to properly call the async function
         await parallel_llm_character_extraction.main(config)
         print("*********************\n\n\n")
 
-# 4. Use asyncio.run() to start the main async function
+
 if __name__ == "__main__":
     asyncio.run(main())
